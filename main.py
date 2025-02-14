0b2ba26e9866a18ab9210bc1f318cef295472985
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2025-02-15 03:07:24 +0530

    updated Class WEBSITE

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import streamlit as st
 
-class WEBSITE:
+class WEBSITE: 
 
     def __init__(self):
-        st.write("Hello")
+        heading=st.container(border=True)
+        heading.title("Visualezee",anchor="home")
 
-    def navigation(self):
-        container = st.container(border=True)
-        container.write("This is inside the container")
-        st.write("This is outside the container")
 
+    def navigation(self,navlist):
+        
+        cols = st.columns(len(navlist))
+        i=0
+        for col in cols:
+            col.button(navlist[i],icon="🎯",use_container_width=True)
+            i+=1
+
+    
+    @st.dialog("Login Form")
+    def open_login_form(self,data):
+        pass
+    
+    def login(self):
+
+
+        pass
+
+    def signup(self):
+        pass
+        
+
+class HOME_PAGE:
+    pass
 
 if __name__=='__main__':
     w = WEBSITE()
-    w.navigation()
+    w.navigation(["Home","Git","Login","SignUp"])
