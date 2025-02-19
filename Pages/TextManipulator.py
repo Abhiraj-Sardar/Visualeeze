e181b5c749dca2a823ff8869fbfe067dc382d3c5
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2025-02-20 00:21:52 +0530

    updated TextManipulator.py

--- Pages/TextManipulator.py
+++ Pages/TextManipulator.py
@@ -1,6 +1,38 @@
 import streamlit as st
-st.write("Text Manipulation")
+import string
+
+st.markdown("## Text Insights")
 
 txt = st.text_area("Enter The Text to get insights")
-n=len(txt)
+translator = str.maketrans('', '', string.punctuation)
+
+# Remove punctuation
+clean_txt = txt.translate(translator)
+txt_length=len(clean_txt)
+
+list_of_words=clean_txt.split()
+no_of_word=len(list_of_words)
+
+
+#count no of punctuation
+cnt_punc=0
+cnt_digit=0
+
+for ch in txt:
+    if ch in string.punctuation:
+        cnt_punc+=1
+    
+    elif ch.isdigit():
+        cnt_digit+=1
+
+container = st.container(border=True)
+container.text(f"No of characters in The Text is : {txt_length}")
+container.text(f"No of words in The Text is : {no_of_word}")
+container.text(f"No of Punctuation in The Text is : {cnt_punc}")
+container.text(f"No of Digit in The Text is : {cnt_digit}")
+
+unique_words=set(list_of_words)
+st.write(clean_txt)
+
+
 
